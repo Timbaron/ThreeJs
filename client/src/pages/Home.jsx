fb537ae53639ea1933a508e9b2ff32acabe6f67f
timbaron <timothy.akiode@student.aul.edu.ng>
2023-04-11 11:38:02 +0100

    creat snapshot with valtio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const snap = useSnapshot(state);
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
