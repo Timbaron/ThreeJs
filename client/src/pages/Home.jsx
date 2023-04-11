ed433eb4274b685ff6089aa4afc9ede6d82f3496
timbaron <timothy.akiode@student.aul.edu.ng>
2023-04-11 12:03:57 +0100

    create custom button component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import {
   headTextAnimation,
   slideAnimation,
 } from "../config/motion";
+import { CustomButton } from "../components";
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -30,14 +31,20 @@ const Home = () => {
               </h1>
             </motion.div>
             <motion.div
-                {...headContentAnimation}
-                className="flex felx-col gap-5"
+              {...headContentAnimation}
+              className="flex felx-col gap-5"
             >
               <p className="max-w-md font-normal text-grey-600 text-base">
                 Create your unique and exclusive shirt with our brand-new 3D
                 customization tool. <strong>Unleash your imagination</strong>{" "}
                 and define your style.
               </p>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
